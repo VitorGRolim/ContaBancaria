7f1245778d7b96ff689ce16db0c0c0fb34c852f5
VitorGRolim <vitorg.rolim@gmail.com>
2023-04-04 15:34:40 -0300

    Proximas Versões

---
+++ ContaBancaria/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace ContaBancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SaldoTotal st;
+
+            Console.WriteLine("INSIRA AS INFORMAÇÕES DA CONTA ABAIXO. ");
+
+            Console.Write("DIGITE O NUMERO DA CONTA: ");
+            int conta = int.Parse(Console.ReadLine());
+            Console.Write("DIGITE O NOME DO TITULAR: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("\nHAVERÁ DEPOSITO INICIAL? (s/n) ");
+            char di = char.Parse(Console.ReadLine());
+            if (di == 's' || di == 'S')            {
+                Console.Write("DIGITE O VALOR DO DEPOSITO INICIAL: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                st = new SaldoTotal(conta, titular, depositoInicial);
+            }
+            else            {
+                st = new SaldoTotal(conta, titular);
+            }
+
+            Console.WriteLine("\nDADOS DA CONTA: \n" + st);
+
+            Console.Write("\nINSIRA O VALOR DO DEPOSITO: ");
+            double dep = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            st.Deposito(dep);
+
+            Console.WriteLine("\nDADOS DA CONTA ATUALIZADOS: \n" + st);
+
+            Console.Write("\nINSIRA O VALOR DO SAQUE: ");
+            dep = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            st.Saque(dep);
+
+            Console.WriteLine("\nDADOS DA CONTA ATUALIZADOS: \n" + st);
+
+        }
+    }
+}
---
+++ ContaBancaria/SaldoTotal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace ContaBancaria
+{
+    internal class SaldoTotal
+    {
+        public int Conta { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public SaldoTotal(int conta, string titular)        {
+            Conta = conta;
+            Titular = titular;
+        }
+        public SaldoTotal(int conta, string titular, double depositoInicial) : this(conta, titular)        {
+            Deposito(depositoInicial);
+        }
+        public void Deposito(double saldo)        {
+            Saldo += saldo;
+        }
+
+        public void Saque(double saldo)        {
+            Saldo -= (saldo + 5.0);
+        }
+        public override string ToString()        {
+            return "Conta: " + Conta
+                + ", Titular: " + Titular
+                + ", Saldo: " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
